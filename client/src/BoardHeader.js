ccb0a8cd2d6157945019e35fb48a5e58e74109d4
Fractal393 <akshatdalmiait2@vjit.ac.in>
2022-05-06 22:25:49 +0530

    changes1

--- client/src/BoardHeader.js
+++ client/src/BoardHeader.js
@@ -1,16 +1,16 @@
 function BoardHeader() {
   return (
     <>
-      <div className="h-20 bg-cover" style={{backgroundImage:'url("https://styles.redditmedia.com/t5_2qs0q/styles/bannerBackgroundImage_7glcgg5ymxp21.png?width=4000&s=9684bc66e812b8730ad694c3f454da8c00a493d7")'}}>
+      <div className="h-20 bg-cover" style={{backgroundImage:'url("https://drive.google.com/thumbnail?id=1DqagJ4r1y0n91gztVF9Dm83wQ-_JZjpe")'}}>
       </div>
       <div className="bg-reddit_dark-brighter">
         <div className="mx-6 relative flex">
           <div className="h-20 w-20 rounded-full overflow-hidden relative -top-3 border-4 border-white bg-white">
-            <img src="https://styles.redditmedia.com/t5_2qs0q/styles/communityIcon_kxcmzy9bt1381.jpg?width=256&format=pjpg&s=0a2e472f6fae0712fee4a3b5d44920fe35dbcdaa" alt=""/>
+            <img src="https://cdn-icons-png.flaticon.com/512/2716/2716646.png" alt=""/>
           </div>
           <div className="pt-2 pl-4">
-            <h1 className="text-gray-300 text-3xl">webdev: reddit for web developers</h1>
-            <h5 className="text-gray-500">r/webdev</h5>
+            <h1 className="text-gray-300 text-3xl">Verite: symposium for Cybersecurity</h1>
+            <h5 className="text-gray-500">v/Cybersecurity</h5>
           </div>
         </div>
       </div>
