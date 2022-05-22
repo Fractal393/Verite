028ea314f05c960a07af5de3c78b1ee7f2ed3334
Fractal393 <akshatdalmiait2@vjit.ac.in>
2022-05-22 13:32:56 +0530

    flag variable created as code

--- client/src/Board.js
+++ client/src/Board.js
@@ -3,6 +3,7 @@ import PostForm from "./PostForm";
 import PostsListing from "./PostsListing";
 
 function Board() {
+  console.log("The flag format is keyword{random_code} LOOK FOR THE KEYWORD AT THE PLACE WHERE YOU CAN USE THE KEY TO ACCESS THIS WORLD.")
   return (<div>
     <BoardHeader />
     <PostForm />
