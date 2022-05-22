c1e96b6c5e09f9de7f38652e268f6bfec959fb97
Abhishek-Raj-Chowdary <arc11832811@gmail.com>
2022-05-22 13:30:08 +0530

    removing flag from login

--- client/src/CommentPage.js
+++ client/src/CommentPage.js
@@ -5,7 +5,7 @@ function CommentPage(props) {
   const commentId = props.match.params.id;
 
   return (
-    <div className="py-4 px-6 bg-verite_light dark:bg-verite_dark ">
+    <div className="py-4 px-6 bg-verite_light dark:bg-verite_dark  ">
       <div className="bg-verite_light dark:bg-verite_dark p-3 rounded-md">
         <Comment id={commentId} />
       </div>
