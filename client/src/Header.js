64b3b8b65fd093cf001f4067358c2b71d9f2bdb0
Abhishek-Raj-Chowdary <arc11832811@gmail.com>
2022-05-22 15:33:52 +0530

    minor text color changes

--- client/src/Header.js
+++ client/src/Header.js
@@ -45,8 +45,8 @@ function Header() {
           <img src={Logo} alt="" className="w-8 h-8 mr-4"/>
         </Link>
         <form onSubmit={doSearch} className="bg-verite_light-brighter  dark:bg-verite_dark-brighter  px-3 flex rounded-md border border-gray-400 mx-4 flex-grow">
-          <SearchIcon className="text-gray-300 dark:bg-verite_dark-brighter h-6 w-6 mt-1" />
-          <input type="text" className="bg-verite_light-brighter dark:bg-verite_dark-brighter  text-sm p-1 pl-2 pr-0 block focus:outline-none text-none"
+          <SearchIcon className="text-gray-400 dark:bg-verite_dark-brighter h-6 w-6 mt-1" />
+          <input type="text" className="bg-verite_light-brighter dark:bg-verite_dark-brighter  text-gray-500 dark:text-gray-400 text-sm p-1 pl-2 pr-0 block focus:outline-none text-none"
                  placeholder="Search"
                  value={searchText}
                  onChange={ev => setSearchText(ev.target.value)}
