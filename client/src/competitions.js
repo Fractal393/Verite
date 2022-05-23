a16fcdc89dbb3bf94811dd7ce37e3d38286e0b55
Abhishek-Raj-Chowdary <arc11832811@gmail.com>
2022-05-23 21:00:53 +0530

    compeition text color

--- client/src/competitions.js
+++ client/src/competitions.js
@@ -23,13 +23,13 @@ function Competitions() {
             </p>
         <p><span class="date font-bold">Approximate Annual Date: March</span></p>
         </div>
-        <div className="bg-verite_light-brighter  dark:bg-verite_dark-brighter   rounded-md my-3 px-6 py-4">
+        <div className="bg-verite_light-brighter  dark:bg-verite_dark-brighter  text-gray-600 rounded-md my-3 px-6 py-4">
         <h1 className=" underline hover:no-underline font-bold  text-xl" ><a href="https://cansecwest.com">PITCOCTF </a></h1>
             <p className="font-semibold text-gray-500 dark:text-gray-400 ">PicoCTF is a cyber security competition available to middle and high school students. The event is put on by Carnegie Mellon University, and places teams into an intereactive environment and storyline where they must hack, decrypt, reverse engineer, and break different sandbox elements. The competition is the largest cyber security competititon available to middle and high schoolers.
             </p>
         <p><span class="date font-bold">Approximate Annual Date: March-April</span></p>
         </div>
-        <div className="bg-verite_light-brighter  dark:bg-verite_dark-brighter   rounded-md my-3 px-6 py-4">
+        <div className="bg-verite_light-brighter  dark:bg-verite_dark-brighter  text-gray-600 rounded-md my-3 px-6 py-4">
         <h1 className=" underline hover:no-underline font-bold  text-xl" ><a href="https://cansecwest.com">CYBERPATRIOT </a></h1>
             <p className="font-semibold text-gray-500 dark:text-gray-400 ">CyberPatriot is a program established for the K-12 education of students in cyber security by the Air Force Association. There are three branches of the program, including the National Youth Cyber Defense Competition, AFA CyberCamps, and Elementary School Cyber Education Initiative. The Cyber Defense Competition starts at the state and then regional level. Top competitors are then given an all-expense paid trip to the national finals in Baltimore, Maryland. At nationals, participants compete for national recognition and scholarship money.</p>
         <p><span class="date font-bold">Approximate Annual Date: Year-Round</span></p>
